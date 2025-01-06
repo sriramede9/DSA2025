b84c4e0f84baa7a64e2f4a08bbafa70d9fe3494e
sriramede9 <only4programming03@gmail.com>
2025-01-06 17:45:39 -0500

    first second largest num in array in single pass

--- src/main/java/org/sri/arrays/SecondLargest.java
+++ src/main/java/org/sri/arrays/SecondLargest.java
@@ -1,20 +1,23 @@
 package org.sri.arrays;
 
 public class SecondLargest {
-    int[] arr = new int[]{1, 6, 3, 5, 7, 0, 9};
-
-    void findSecondLargest() {
+    public static int findSecondLargest(int[] arr) {
         int firstMax = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
 
-        for (int i = 0; i < arr.length; i++) {
-
-            if(arr[i]>firstMax){
-               firstMax=arr[i];
-                //if first max is greater than
+        for (int j : arr) {
+            if (j > firstMax) {
+                secondMax = firstMax; // if the horse is running faster than firstMax, this is my new max and current first is my secondMax
+                firstMax = j;
+            } else if (j < firstMax && j > secondMax) { // if the horse is running slower than first and faster than second, this is my secondMax
+                secondMax = j;
             }
         }
+        return secondMax;
+    }
 
-
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,8,4,3,7,5};
+        System.out.println(findSecondLargest(nums));
     }
 }
